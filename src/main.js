15e67ce67645aaa08fb299f824176807976ac9d9
Masayoshi Wada <developer@andantesoftware.com>
2015-12-21 16:50:12 +0900

    format arrow function

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { ext as ext } from "./ext.js"
   // codes from https://github.com/khanning/scratch-arduino-extension/blob/da1ab317a215a8c1c5cda1b9db756b9edc14ba68/arduino_extension.js#L533-L541
   let variables = window.location.search.replace(/^\?|\/$/g, '').split('&')
   var lang = 'en';
-  variables.forEach(variable => {
+  variables.forEach((variable) => {
     let pair = variable.split('=')
     if (pair.length > 1 && pair[0] == 'lang')
       lang = pair[1];
